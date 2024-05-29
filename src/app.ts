1ffb508545f1b4a9097073a3e474cdea380134dc
Sujin Jeon <sjjeon@enki.co.kr>
2024-05-29 18:30:21 +0900

    Feat build development environment

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,9 @@
-export const sum = (a: number, b: number) => {
-	return a + b;
-}
+import express from "express";
+import * as dotenv from "dotenv";
+dotenv.config({ path: "../" + __dirname + "/.env.local" });
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
