48c1b563c5e39ae327ec1411465d88ebd3067224
Sujin Jeon <sjjeon@enki.co.kr>
2024-06-05 11:49:19 +0900

    Feat env file configuration function

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,25 +1,15 @@
 import path from 'path';
 import dotenv from 'dotenv';
+import { configureEnvFile } from '../utils/envFile';
 
 /**
  * Server Config
  */
 export const NODE_ENV = process.env.NODE_ENV || 'development';
 export const ROOT_PATH = path.resolve(__dirname, '../../');
-export const ENV_FILE = () => {
-  switch (NODE_ENV) {
-    case 'production':
-      return '.env.prod';
-    case 'development':
-      return '.env.dev';
-    case 'test':
-      return '.env.test';
-    default:
-      return '.env';
-  }
-};
+export const ENV_FILE = configureEnvFile();
 dotenv.config({ path: `${ROOT_PATH}/${ENV_FILE}` });
-export const SKIP_ENV_TEST = process.env.SKIP_ENV_TEST === 'true';
+export const SKIP_ENV_TEST = Boolean(process.env.SKIP_ENV_TEST);
 export const PORT = Number(process.env.PORT) || 3000;
 
 /**
