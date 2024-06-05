9717e4172a81d7e03d4a018b0fe9a8df538fdd49
Sujin Jeon <sjjeon@enki.co.kr>
2024-06-05 11:28:14 +0900

    Fix env files

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,19 @@ import dotenv from 'dotenv';
  */
 export const NODE_ENV = process.env.NODE_ENV || 'development';
 export const ROOT_PATH = path.resolve(__dirname, '../../');
-dotenv.config({ path: `${ROOT_PATH}/.env.${NODE_ENV}` });
+export const ENV_FILE = () => {
+  switch (NODE_ENV) {
+    case 'production':
+      return '.env.prod';
+    case 'development':
+      return '.env.dev';
+    case 'test':
+      return '.env.test';
+    default:
+      return '.env';
+  }
+};
+dotenv.config({ path: `${ROOT_PATH}/${ENV_FILE}` });
 export const SKIP_ENV_TEST = process.env.SKIP_ENV_TEST === 'true';
 export const PORT = Number(process.env.PORT) || 3000;
 
